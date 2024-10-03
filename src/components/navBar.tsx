a22488472787505eed5c6616109136bb9d788413
arianrdcas <arianrdcas@gmail.com>
2024-10-03 18:49:04 -0300

    Mejorando estilos y funciones del navbar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,103 +1,53 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 const Navbar: FC = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = isDarkTheme ? "dark" : "light";
+  }, [isDarkTheme]);
+
   return (
     <div className="flex justify-between p-3 items-center relative">
       {/* Contenido del Navbar */}
-
       <div className="relative">
-        <svg
-          className="absolute right-5 top-1/2 transform -translate-y-1/2 text-white "
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.03.062.058.098.083a1.007 1.007 0 0 0 1.416 0 1.13 1.13 0 0 0 .083-.098l3.85 3.85a1 1 0 1 0 1.415-1.414l-3.85-3.85zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-        </svg>
+        <SearchOutlinedIcon className="absolute right-5 top-1/2 transform -translate-y-1/2 text-white " />
         <input
           type="text"
           placeholder="Search"
-          className="rounded bg-gray-600 text-white placeholder-white pl-3 pr-4 py-1 w-full outline-none"
+          className="border border-transparent rounded bg-gray-600 text-white placeholder-white pl-3 pr-4 py-1 w-full focus:border-white focus:outline-none"
         />
       </div>
-
-      <div className="flex justify-around py-3 w-52 items-center ">
-        <svg
-          className="w-6 h-6 text-white transform-transition hover:scale-110"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
+      <div className="flex justify-around py-3 w-52 items-center">
+        <button
+          className="transition duration-300 hover:scale-125"
+          onClick={toggleTheme}
         >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1"
-            d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"
-          />
-        </svg>
-
-        <svg
-          className="w-[24px] h-[24px] text-white transform-transition hover:scale-110"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.1"
-            d="M12 5.365V3m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175 0 .593 0 1.292-.538 1.292H5.538C5 18 5 17.301 5 16.708c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 12 5.365ZM8.733 18c.094.852.306 1.54.944 2.112a3.48 3.48 0 0 0 4.646 0c.638-.572 1.236-1.26 1.33-2.112h-6.92Z"
-          />
-        </svg>
-        <svg
-          className="w-[24px] h-[24px] text-white transform-transition hover:scale-110"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.1"
-            d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"
-          />
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.1"
-            d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-          />
-        </svg>
-        <svg
-          className="w-[29px] h-[29px] text-white transform-transition hover:scale-110"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            strokeWidth="1"
-            d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
+          {isDarkTheme ? (
+            <DarkModeOutlinedIcon className="w-6 h-6 text-white" />
+          ) : (
+            <WbSunnyOutlinedIcon className="w-6 h-6 text-white" />
+          )}
+        </button>
+        <button className="transition duration-300 hover:scale-125">
+          <NotificationsNoneOutlinedIcon className="w-6 h-6 text-white" />
+        </button>
+        <button className="transition duration-300 hover:scale-125">
+          <SettingsOutlinedIcon className="w-6 h-6 text-white" />
+        </button>
+        <button className="transition duration-300 hover:scale-125">
+          <PersonOutlineOutlinedIcon className="w-6 h-6 text-white" />
+        </button>
       </div>
     </div>
   );
